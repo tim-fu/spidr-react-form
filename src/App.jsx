625db650b2cf276eaec428cd4e02564a022caa2e
Tim Fu <91704684+tim-fu@users.noreply.github.com>
2025-07-16 17:26:51 -0700

    Add labels

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
 import { useForm } from "react-hook-form";
 
+const SECRET_PIN_LENGTH = 4;
+
 export function App() {
-  const { register, handleSubmit } = useForm();
+  const { 
+    register, 
+    handleSubmit,
+    formState: { errors } } = useForm({
+      defaultValues: {
+        firstName: '',
+        lastName: '',
+        phone: '',
+        email: '',
+        fryerCost: '',
+        secretPin: '',
+      }
+    });
+
+    console.log(errors)
 
   return (
     <main>
       <form onSubmit={handleSubmit((data) => {
         console.log(data);
       })}>
-        <input {...register("firstName")} placeholder="First name" />
-        <input {...register("lastName")} placeholder="Last name" />
-        <input {...register("phone")} placeholder="Phone" />
-        <input {...register("email")} placeholder="Email" />
-        <input {...register("fryerCost")} placeholder="Fryer cost" />
-        <input {...register("secretPIN")} placeholder="Spidr PIN" />
+        <label>First Name
+          <input {...register("firstName", { required: true })} placeholder="e.g. Peter" />
+        </label>
+        <label>Last Name
+          <input {...register("lastName", { required: true })} placeholder="e.g. Parker" />
+        </label>
+        <label>Phone
+          <input {...register("phone", { required: true })} placeholder="e.g. 630-383-2070" />
+        </label>
+        <label>Email
+          <input {...register("email", { required: true })} placeholder="e.g. spiderman@spidr.com" />
+        </label>
+        <label>Fryer Cost
+         <input {...register("fryerCost", { required: true })} placeholder="e.g. $1000" />
+        </label>
+        <label>Spidr PIN
+          <input {...register("secretPin", 
+            { required: true, 
+              minLength: {value: SECRET_PIN_LENGTH, message: `Length is ${SECRET_PIN_LENGTH}`},
+              maxLength: {value: SECRET_PIN_LENGTH, message: `Length is ${SECRET_PIN_LENGTH}`} })} placeholder="Your secret 16-digit code" />
+        </label>
         <input type="submit" />
       </form>
     </main>

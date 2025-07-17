06fb434abec5dce0f082fb74ad4a3f7410af793e
Tim Fu <91704684+tim-fu@users.noreply.github.com>
2025-07-17 03:46:19 -0700

    Clean up code, remove extra functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
 
-const SECRET_PIN_LENGTH = 4;
+const SECRET_PIN_LENGTH = 19; // including hyphens
 
 export function App() {
   const { 
@@ -17,7 +17,7 @@ export function App() {
       }
     });
 
-    console.log(errors)
+  console.log(errors);
 
   return (
     <main>
@@ -25,7 +25,7 @@ export function App() {
         console.log(data);
       })}>
         <h1>Interest Form</h1>
-        <h3>Fill this out for a brand-new, legendary air fryer!</h3>
+        <h3>Who's ready for a top-of-the-line air fryer? You are!</h3>
 
         <label>First Name
           <input {...register("firstName", { required: true })} placeholder="e.g. Peter" />
@@ -33,20 +33,22 @@ export function App() {
         <label>Last Name
           <input {...register("lastName", { required: true })} placeholder="e.g. Parker" />
         </label>
-        <label>Phone
-          <input {...register("phone", { required: true })} placeholder="e.g. 630-383-2070" />
+        <label>Phone Number
+          <input type="tel" {...register("phone", { required: true })} placeholder="e.g. 630-383-2070" />
         </label>
         <label>Email
-          <input {...register("email", { required: true })} placeholder="e.g. spiderman@spidr.com" />
+          <input type="email" {...register("email", { required: true })} placeholder="e.g. spiderman@spidr.com" />
         </label>
-        <label>Fryer Cost
+        <label>Guess the Fryer Cost
          <input {...register("fryerCost", { required: true })} placeholder="e.g. $1000" />
         </label>
-        <label>Spidr PIN
-          <input {...register("secretPin", 
-            { required: true, 
-              minLength: {value: SECRET_PIN_LENGTH, message: `Length is ${SECRET_PIN_LENGTH}`},
-              maxLength: {value: SECRET_PIN_LENGTH, message: `Length is ${SECRET_PIN_LENGTH}`} })} placeholder="Your secret 16-digit code" />
+        <label>Your Top Secret Spidr PIN
+          <input
+            {...register("secretPin", 
+              { required: true, 
+                minLength: {value: SECRET_PIN_LENGTH, message: `Length is ${SECRET_PIN_LENGTH}`},
+                maxLength: {value: SECRET_PIN_LENGTH, message: `Length is ${SECRET_PIN_LENGTH}`} })} 
+            placeholder="e.g. 1234-5678-1234-5678"/>
         </label>
         <div><button type="submit">Submit</button></div>
       </form>
